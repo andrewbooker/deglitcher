7c93a059c77177f9d7dccaf094c9f2ba32005d75
Andrew Booker <andrew.booker@acuris.com>
2019-08-15 13:18:14 +0100

    simulating glitch caused by recording dropout

--- generate_example.py
+++ generate_example.py
@@ -4,10 +4,13 @@ import soundfile as sf
 import math
 
 sampleRate = 44100
+breakpoint = 19632
+dropout = 1937
 
 def generate(f):
     with sf.SoundFile("./audio.wav", mode="x", samplerate=44100, channels=1, subtype="PCM_24") as file:
         for i in range(sampleRate):
-            file.write(math.sin(i * f * 2 * math.pi / sampleRate))
+            idx = i if i < breakpoint else i + dropout
+            file.write(math.sin(idx * f * 2 * math.pi / sampleRate))
 
-generate(1)
+generate(100)
